3f92a103da13097e08af10fb6152b69364715c67
yanbo <2313567416@qq.com>
2018-11-24 18:41:36 +0800

    save

---
+++ HHplayerText/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HHplayerText
+//
+//  Created by po yan on 2018/11/24.
+//  Copyright © 2018年 po yan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HHplayerText/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  HHplayerText
+//
+//  Created by po yan on 2018/11/24.
+//  Copyright © 2018年 po yan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
